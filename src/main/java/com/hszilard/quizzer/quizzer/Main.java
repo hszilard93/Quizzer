2dc6d82da27153cd6ffacf9fef1c65d7e8033260
Szilard Hompoth <hompoth.szilard@gmail.com>
2018-01-10 22:48:01 +0200

    Executables, missing main method in Quizzer

--- src/main/java/com/hszilard/quizzer/quizzer/Main.java
+++ src/main/java/com/hszilard/quizzer/quizzer/Main.java
@@ -42,4 +42,9 @@ public class Main extends Application {
         iconify(primaryStage);
         primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
